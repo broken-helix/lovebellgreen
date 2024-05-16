f65b395352d9a2a1b0a364f5d56d55106ec77ca4
broken-helix <james_e75@hotmail.com>
2024-05-16 22:36:55 +0100

    add image with text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {
+  myFunction();
+};
 
 // Get the navbar
 var navbar = document.getElementById("site-navbar");
@@ -7,12 +9,18 @@ var navbar = document.getElementById("site-navbar");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
+// Get the height of the navbar
+var navbarHeight = navbar.offsetHeight;
+
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  console.log("Sticky");
   if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
+    // Add padding to the top of the page content equal to the height of the navbar
+    document.body.style.paddingTop = navbarHeight + "px";
   } else {
     navbar.classList.remove("sticky");
+    // Reset padding when the navbar is not sticky
+    document.body.style.paddingTop = 0;
   }
 }
